ef44ec9ad9cd6873e54231375c2e48cdb5e83b93
Alexandre BABEU <alexandrebabeu@gmail.com>
2017-08-08 16:09:08 +0200

    Storyboard + Controllers updates

---
+++ FitTime/ProfilViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfilViewController.swift
+//  FitTime
+//
+//  Created by Alexandre Babeu on 08/08/2017.
+//  Copyright © 2017 Alexandre BABEU. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ProfilViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var profilImg: UIImageView!
+    @IBOutlet weak var tblView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+        
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
